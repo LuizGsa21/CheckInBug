0ea6b03d5d7fa6674a95422fa6705644ee5da672
sbowen <sbowen@architectnow.net>
2023-09-18 09:26:14 -0500

    GPT fixes

--- frontend/CheckInApp/App.tsx
+++ frontend/CheckInApp/App.tsx
@@ -5,96 +5,91 @@
  * @format
  */
 
-import React, {FC, useEffect, useState} from 'react';
-import {
-  Alert,
-  AppState,
-  AppStateStatus,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, { FC, useEffect, useState } from 'react';
+import { Alert, AppState, AppStateStatus, Button, Text, View } from 'react-native';
+import { UserInfo, UserService } from './user.service';
 
-import {UserInfo, UserService} from './user.service';
-
-function forceRenderHook() {
-  const [count, setCount] = useState(0);
-  return () => setCount(count + 1);
-}
-
-const HomeScreen: FC<{userInfo: UserInfo}> = ({userInfo}) => {
-  const forceRender = forceRenderHook();
+const HomeScreen: FC<{
+  userInfo: UserInfo | null;
+  onCheckIn: () => Promise<void>;
+  onCheckOut: () => Promise<void>;
+}> = ({ userInfo, onCheckIn, onCheckOut }) => {
   const canCheckIn =
-    userInfo.lastCheckIn == null ||
+    userInfo?.lastCheckIn == null ||
     (userInfo.lastCheckIn.timeIn != null &&
       userInfo.lastCheckIn.timeOut != null);
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: canCheckIn ? 'red' : 'green'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: canCheckIn ? 'red' : 'green' }}>
         {canCheckIn ? 'User is checked-out' : 'User is checked-in'}
       </Text>
-      <Text style={{color: canCheckIn ? 'red' : 'green'}}>
+      <Text style={{ color: canCheckIn ? 'red' : 'green' }}>
         {JSON.stringify(userInfo || {}, null, 4)}
       </Text>
       {canCheckIn && (
-        <Button
-          onPress={() => {
-            UserService.CheckIn().then(response => {
-              if (response.success) {
-                userInfo!.lastCheckIn = response.data;
-                forceRender();
-              } else {
-                Alert.alert(response.data || 'Unknown error');
-                if (response.data === 'You must be checked in to checkout') {
-                  return UserService.UserInfo().then(r => {
-                    if (r.success) {
-                      userInfo!.lastCheckIn = r.data.lastCheckIn;
-                      forceRender();
-                    }
-                  });
-                }
-              }
-            });
-          }}
-          title={'Check-In'}
-        />
+        <Button onPress={onCheckIn} title={'Check-In'} />
       )}
-
       {!canCheckIn && (
-        <Button
-          onPress={() => {
-            UserService.CheckOut().then(response => {
-              if (response.success) {
-                userInfo!.lastCheckIn = response.data;
-                forceRender();
-              } else {
-                Alert.alert(response.data || 'Unknown error');
-                if (response.data === 'You were forced checkout') {
-                  return UserService.UserInfo().then(r => {
-                    if (r.success) {
-                      userInfo!.lastCheckIn = r.data.lastCheckIn;
-                      forceRender();
-                    }
-                  });
-                }
-              }
-            });
-          }}
-          title={'Checkout'}
-        />
+        <Button onPress={onCheckOut} title={'CheckOut'} />
       )}
     </View>
   );
 };
 
-function App(): JSX.Element {
-  const [userInfo, setUserInfo] = useState({});
+const App: FC = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  const handleCheckIn = async () => {
+    const response = await UserService.CheckIn();
+    if (response.success) {
+        setUserInfo(prevUserInfo => ({
+            ...prevUserInfo,
+            email: prevUserInfo?.email || "", // provide default value if undefined
+            lastCheckIn: response.data
+        }));
+    } else {
+      Alert.alert(response.data || 'Unknown error');
+      if (response.data === 'You must be checked in to checkout') {
+          const r = await UserService.UserInfo();
+          if (r.success) {
+              setUserInfo(prevUserInfo => ({
+                  ...prevUserInfo,
+                  email: prevUserInfo?.email || "",  // Ensure email field is always set
+                  lastCheckIn: r.data.lastCheckIn
+              }));
+          }
+      }
+  }
+  
+};
+
+const handleCheckOut = async () => {
+  const response = await UserService.CheckOut();
+  if (response.success) {
+      setUserInfo(prevUserInfo => ({
+          ...prevUserInfo,
+          email: prevUserInfo?.email || "",  // Ensure email field is always set
+          lastCheckIn: response.data
+      }));
+  } else {
+      Alert.alert(response.data || 'Unknown error');
+      if (response.data === 'You were forced checkout') {
+          const r = await UserService.UserInfo();
+          if (r.success) {
+              setUserInfo(prevUserInfo => ({
+                  ...prevUserInfo,
+                  email: prevUserInfo?.email || "",  // Ensure email field is always set
+                  lastCheckIn: r.data.lastCheckIn
+              }));
+          }
+      }
+  }
+};
+
 
   useEffect(() => {
     const appStateChangedHandler = async (state: AppStateStatus) => {
-      console.log('AppStateStatus', state);
       if (state === 'active') {
         const r = await UserService.UserInfo();
         if (r.success) {
@@ -105,14 +100,13 @@ function App(): JSX.Element {
       }
     };
     const handler = AppState.addEventListener('change', appStateChangedHandler);
-    // app launch
     appStateChangedHandler(AppState.currentState);
     return () => {
       handler.remove();
     };
   }, []);
 
-  return <HomeScreen userInfo={userInfo} />;
-}
+  return <HomeScreen userInfo={userInfo} onCheckIn={handleCheckIn} onCheckOut={handleCheckOut} />;
+};
 
 export default App;
